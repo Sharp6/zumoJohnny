d3287af05d9ffa66f12dff407217ed94ea7aa28e
Philip Huysmans <philip.huysmans@gmail.com>
2016-08-09 20:43:59 +0200

    ok, fixed

--- sandboxing/monitorServer.js
+++ sandboxing/monitorServer.js
@@ -13,32 +13,23 @@ board.on("ready", function() {
 	joystick = new AnalogJoystick(new Nunchuk(board,five));
 
 	joystick.on("stickMove", function() {
-  		io.emit("stickExtremesReport", joystick.extremes);              
-  		io.emit("normalizedPositionReport", joystick.normalizedPosition);
-  		io.emit("rawPositionReport", joystick.rawPosition);              
+		io.emit("stickExtremesReport", joystick.extremes);
+		io.emit("normalizedPositionReport", joystick.normalizedPosition);
+		io.emit("rawPositionReport", joystick.rawPosition);
+	});
 
+	joystick.on("fireButton", function(state) {
+		io.emit("fireButton", state);
 	});
 });
 
-app.get('/', function(req, res){
-
-
-);                                                                  res.sendFile(__dirname +'/index.html');
-                                                                   });
-joystick.on("fireButton", function(state) {
-  io.emit("fireButton", state);                                    io.on('connection', function(socket){
-});                                                                  console.log('a user connected');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname +'/index.html');
 });
 
-
-
-
-
-
-
-
-
-
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
 
 http.listen(1804, function(){
   console.log('listening on *:1804');
