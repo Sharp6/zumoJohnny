2f8e351fc90a7e5c9979b75584ba1bc94b6a9966
Philip Huysmans <philip.huysmans@gmail.com>
2016-08-09 18:37:06 +0000

    pls fix

--- sandboxing/monitorServer.js
+++ sandboxing/monitorServer.js
@@ -3,26 +3,42 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var TestDriver = require('./TestDriver');
-var AnalogJoystick = require('./../AnalogJoystick');
-var joystick = new AnalogJoystick(new TestDriver());
+var Nunchuk = require("./../nunchuckJoystick");
+var AnalogJoystick = require("./../AnalogJoystick");
+var joystick;
+
+var five = require("johnny-five");
+var board = new five.Board();
+board.on("ready", function() {
+	joystick = new AnalogJoystick(new Nunchuk(board,five));
+
+	joystick.on("stickMove", function() {
+  		io.emit("stickExtremesReport", joystick.extremes);              
+  		io.emit("normalizedPositionReport", joystick.normalizedPosition);
+  		io.emit("rawPositionReport", joystick.rawPosition);              
+
+	});
+});
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname +'/index.html');
-});
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
 
-joystick.on("stickMove", function() {
-  io.emit("stickExtremesReport", joystick.extremes);
-  io.emit("normalizedPositionReport", joystick.normalizedPosition);
-  io.emit("rawPositionReport", joystick.rawPosition);
-});
-
+);                                                                  res.sendFile(__dirname +'/index.html');
+                                                                   });
 joystick.on("fireButton", function(state) {
-  io.emit("fireButton", state);
+  io.emit("fireButton", state);                                    io.on('connection', function(socket){
+});                                                                  console.log('a user connected');
 });
+
+
+
+
+
+
+
+
+
+
 
 http.listen(1804, function(){
   console.log('listening on *:1804');
