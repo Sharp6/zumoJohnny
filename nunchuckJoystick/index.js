32a0d5d59e331e2ddcbc3422e317fa178b219953
Philip Huysmans <philip.huysmans@gmail.com>
2016-08-09 20:46:03 +0200

    fixed

--- nunchuckJoystick/index.js
+++ nunchuckJoystick/index.js
@@ -2,7 +2,7 @@ var NunchukJoystick = function(board, five) {
 	this.joystick = new five.Wii.Nunchuk({ board: board, freq: 50 });
 	this.fireButton = "z";
 
-	nunchuk.joystick.on("change", function(event) {
+	this.joystick.on("change", function(event) {
 		this.emit("move", event.target[event.axis], event.axis);
 
 		/*
@@ -14,13 +14,13 @@ var NunchukJoystick = function(board, five) {
 		*/
   });
 
-	nunchuk.on("down", function(event) {
+	this.joystick.on("down", function(event) {
 		if(event.target.which === this.fireButton) {
 			this.emit("fireButton", "on");
 		}
 	});
 
-	nunchuk.on("up", function(event) {
+	this.joystick.on("up", function(event) {
 		if(event.target.which === this.fireButton) {
 			this.emit("fireButton", "off");
 		}
