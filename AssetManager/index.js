b0f1c1d80810c5a812e7089806771bf32a3fc24e
Philip Huysmans <philip.huysmans@gmail.com>
2016-11-02 11:19:31 +0100

    assets

--- AssetManager/index.js
+++ AssetManager/index.js
@@ -15,7 +15,7 @@ function Asset(data) {
 	assetClient.on('message', function(topic, message) {
 		console.log("Got assetMessage", message.toString(), "on topic", topic);
 		var msgObj = {};
-		message.split(":").forEach(function(keyValue) {
+		message.toString().split(":").forEach(function(keyValue) {
 			msgObj[keyValue[0]] = keyValue[1];
 		});
 
